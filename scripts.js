92d906e64cb4ea6792e96c7ff15bb0e9fb2271f5
Bogusz Mędykowski <bogusz.medykowski@gmail.com>
2023-12-31 18:05:15 +0100

    black holes

--- scripts.js
+++ scripts.js
@@ -14,3 +14,38 @@ function openTab(evt, tabName) {
 
 // Opcjonalnie, możesz ustawić jedną z zakładek jako domyślnie otwartą:
 document.getElementsByClassName("tablinks")[0].click(); // Otwiera pierwszą zakładkę
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    function createRandomElement() {
+        const element = document.createElement('div');
+        element.classList.add('element');
+
+        // Losowa pozycja i rozmiar
+        element.style.position = 'absolute'; // Ustawienie pozycji absolutnej
+        element.style.left = Math.random() * document.body.clientWidth + 'px';
+        element.style.top = Math.random() * document.body.clientHeight + 'px';
+        element.style.width = '20px';
+        element.style.height = '20px';
+
+        document.body.appendChild(element); // Dodanie elementu do <body>
+
+        // Funkcja rosnąca
+        let size = 20;
+        const grow = setInterval(function() {
+            size++;
+            element.style.width = size + 'px';
+            element.style.height = size + 'px';
+            if (size >= 100) clearInterval(grow); // Zatrzymaj rośnięcie
+        }, 100);
+
+        // Reakcja na najechanie kursorem
+        element.addEventListener('mouseover', function() {
+            clearInterval(grow); // Zatrzymaj rośnięcie
+            element.remove(); // Usuń element
+        });
+    }
+
+    // Tworzenie nowego elementu co jakiś czas
+    setInterval(createRandomElement, 2000); // Co 2 sekundy
+});
