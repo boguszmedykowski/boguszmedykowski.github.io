1b1a19930e235650e62bf466eb9806b95a4a4b19
Bogusz Mędykowski <bogusz.medykowski@gmail.com>
2024-01-07 16:40:06 +0100

    add shirime eye

--- scripts.js
+++ scripts.js
@@ -15,24 +15,61 @@ function openTab(evt, tabName) {
 document.getElementsByClassName("tablinks")[0].click(); // Otwiera pierwszą zakładkę
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    const shirimeButton = document.querySelector('.shirime');
+    const eye = document.getElementById('bigEye');
+
+    shirimeButton.addEventListener('click', function () {
+        if (eye.classList.contains('eye-closed')) {
+            eye.classList.remove('eye-closed');
+            eye.classList.add('eye-open');
+        } else {
+            eye.classList.remove('eye-open');
+            eye.classList.add('eye-closed');
+        }
+    });
+});
+
+
 
 // czarne dziury
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     let elementCount = 0; // Licznik śledzący ilość elementów na stronie
-    let removedCount = 0; // Licznik śledzący ilość usuniętych elementów
+    let removedCount = 13; // Licznik śledzący ilość usuniętych elementów
 
     function updateRemovedCounter() {
         const counterElement = document.getElementById('removedCounter');
         counterElement.textContent = `${removedCount}`;
+
+        // Zaktualizuj styl liter w .shirime
+        let shirimeLetters = document.querySelectorAll('.shirime span');
+        if (removedCount > 0 && removedCount <= shirimeLetters.length) {
+            let letterToChange = shirimeLetters[shirimeLetters.length - removedCount];
+            letterToChange.style.color = 'white';
+            letterToChange.style.textShadow = '0 0 10px white, 0 0 20px white, 0 0 30px white, 0 0 40px white, 0 0 50px white, 0 0 60px white, 0 0 70px white';
+        }
     }
+    function updateEye() {
+        const eye = document.getElementById('bigEye');
+        if (eye) {
+            if (removedCount === 0) {
+                eye.classList.remove('eye-closed');
+                eye.classList.add('eye-open');
+            } else {
+                eye.classList.remove('eye-open');
+                eye.classList.add('eye-closed');
+            }
+        }
+    }
+
 
     // Inicjalizacja licznika usuniętych elementów na początku
     updateRemovedCounter();
 
     function createRandomElement() {
         // Tworzy element tylko jeśli jest mniej niż 5 elementów
-        if (elementCount < 5) {
+        if (elementCount < 5 && removedCount > 0) {
             const element = document.createElement('div');
             element.classList.add('element');
 
@@ -48,26 +85,34 @@ document.addEventListener('DOMContentLoaded', function() {
 
             // Funkcja rosnąca
             let size = 20;
-            const grow = setInterval(function() {
+            const grow = setInterval(function () {
                 size++;
                 element.style.width = size + 'px';
                 element.style.height = size + 'px';
             }, 100);
 
             // Reakcja na najechanie kursorem
-            element.addEventListener('mouseover', function() {
+            element.addEventListener('mouseover', function () {
                 clearInterval(grow);
                 element.remove();
                 elementCount--;
-                removedCount++; // Zwiększenie licznika usuniętych elementów
+                if (removedCount > 0) {
+                    removedCount--;
+                }; // Zwiększenie licznika usuniętych elementów
                 updateRemovedCounter(); // Aktualizacja licznika na stronie
             });
         }
     }
 
-    // Tworzenie nowego elementu co jakiś czas
+    // Utwórz pierwszy element natychmiast po załadowaniu strony
+    createRandomElement();
+
+    // Następnie, twórz nowe elementy co 6 sekund
     setInterval(createRandomElement, 6000); // Co 6 sekund
 });
+
+
+
 
 
 
